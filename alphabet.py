1e8521dfe7cad937bcfa0f368b1f0344ef576bb7
PAVANANUTHALAPATI <42571590+PAVANANUTHALAPATI@users.noreply.github.com>
2018-08-23 09:38:24 -0700

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+king=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","u","v","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+n19=raw_input("")
+if (n19 in king):
+  print("Alphabet")
+else:
+  print("No")
