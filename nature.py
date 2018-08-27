a9a2235ad3433fd6acd92faff5eadcd8273817e4
PAVANANUTHALAPATI <42571590+PAVANANUTHALAPATI@users.noreply.github.com>
2018-08-27 16:06:59 +0530

    Update nature.py

--- nature.py
+++ nature.py
@@ -1,6 +1,6 @@
-pmav=int(input(""))
-pavala = 0
-while(pmav > 0):
-  pavala=pavala+pmav
-  pmav=pmav-1
-  print(pavala) 
+indra=int(input(""))
+reddy = 0
+while(indra > 0):
+  reddy=reddy+indra
+  indra=indra-1
+  print("reddy") 
