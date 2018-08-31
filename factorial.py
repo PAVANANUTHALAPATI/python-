fe5de2eab00239e474e223f794a336346881ba3b
PAVANANUTHALAPATI <42571590+PAVANANUTHALAPATI@users.noreply.github.com>
2018-08-31 15:40:42 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(indra):
+    if indra == 1:
+        return indra
+    else:
+        return indra * factorial(indra - 1)
+indra = int(input(""))
+if indra < 0:
+    print("Factorial cannot be found for negative numbers")
+elif indra == 0:
+    print("Factorial of 0 is 1")
+else:
+    print( factorial(indra))
