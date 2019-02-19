45b2a7507924ce645eb55d4af32f91ea2ac3d719
PAVANANUTHALAPATI <42571590+PAVANANUTHALAPATI@users.noreply.github.com>
2019-02-19 10:06:22 +0530

    Update letter.py

--- letter.py
+++ letter.py
@@ -1,10 +1,2 @@
-def char_frequency(pavana):
-    dict = {}
-    for n in pavana:
-        keys = dict.keys()
-        if n in keys:
-            dict[n] += 1
-        else:
-            dict[n] = 1
-    return dict
-print(char_frequency('ishana'))
+s="pavana"
+print len(s)
